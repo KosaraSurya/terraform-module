ac4a101045095bf4bbfd93b05415bda2404cae5b
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-24 21:30:28 +0530

    terraform module

---
+++ roboshop-ec2-test/variable.tf
@@ -0,0 +1,16 @@
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+  
+}
+
+variable "security_group_ids" {
+    default = ["sg-0ccba06af95f41fdb"]
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
